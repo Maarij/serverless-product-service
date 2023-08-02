8d79274b6d8c3f44fc389ec5c78c59c3294d526c
Maarij <maarij.anwar@gmail.com>
2023-08-02 18:20:49 -0500

    base project

---
+++ lib/aws-microservices-stack.ts
@@ -0,0 +1,22 @@
+import {RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {AttributeType, BillingMode, Table} from "aws-cdk-lib/aws-dynamodb";
+
+export class AwsMicroservicesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+
+
+  }
+}
