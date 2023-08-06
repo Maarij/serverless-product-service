64350832d1069b12414aad6bb3fe7fa650d87da6
Maarij <maarij.anwar@gmail.com>
2023-08-06 09:06:38 -0500

    refactor & clean up

--- lib/database.ts
+++ lib/database.ts
@@ -5,16 +5,42 @@ import {RemovalPolicy} from "aws-cdk-lib";
 export class CompanyDatabase extends Construct {
 
   public readonly productTable: ITable;
+  public readonly basketTable: ITable;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.productTable = new Table(this, 'product', {
+    this.productTable = this.createProductTable();
+    this.basketTable = this.createBasketTable();
+  }
+
+  private createProductTable(): ITable {
+    // Product table
+    // PK: id
+    // name, description, imageFile, price, category
+    return new Table(this, 'product', {
       partitionKey: {
         name: 'id',
         type: AttributeType.STRING
       },
       tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+  }
+
+  private createBasketTable() {
+    // Basket table
+    // PK: username
+    // items (set map)
+    //   item1 -> {quantity, color, price, productId, productName}
+    //   item2 -> {quantity, color, price, productId, productName}
+    return new Table(this, 'basket', {
+      partitionKey: {
+        name: 'username',
+        type: AttributeType.STRING
+      },
+      tableName: 'basket',
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST
     });
