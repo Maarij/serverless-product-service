48ec56e22543aa5ec13d4d1ec6b6ff8902410af3
Maarij <maarij.anwar@gmail.com>
2023-08-17 20:07:46 -0500

    eventbridge order implementation

--- src/order/index.js
+++ src/order/index.js
@@ -1,9 +1,48 @@
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+
 exports.handler = async function (event) {
   console.log("request: ", JSON.stringify(event, undefined, 2));
 
-  return {
-    statusCode: 200,
-    headers: {"Content-Type": "text/plain"},
-    body: `Hello from ordering. You've hit ${event.path}`
-  };
+  const eventType = event['detail-type'];
+  if (eventType !== undefined) {
+    await eventBridgeInvocation(event); // async
+  } else {
+    return await apiGatewayInvocation(event); // sync
+  }
 };
+
+const eventBridgeInvocation = async (event) => {
+  console.log(`eventBridgeInvocation function. event: "${event}"`);
+
+  // create order item into db
+  await createOrder(event.detail);
+};
+
+const apiGatewayInvocation = (event) => {
+
+};
+
+const createOrder = async (basketCheckoutEvent) => {
+  try {
+    console.log(`createOrder function. event: "${basketCheckoutEvent}"`);
+
+    // set orderDate for SK of order dynamodb
+    basketCheckoutEvent.orderDate = new Date().toISOString();
+    console.log(basketCheckoutEvent);
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(basketCheckoutEvent || {})
+    }
+
+    const createResult = await ddbClient.send(new PutItemCommand(params));
+    console.log(createResult);
+
+    return createResult;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
